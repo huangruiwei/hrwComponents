4d46dba1c7238fcd12e8f2a1631d0b12b59a9ccc
huangruiwei <18814379697@163.com>
2018-08-31 18:17:42 +0800

    新增路由、首页

---
+++ src/components/comment/comHeader.vue
@@ -0,0 +1,20 @@
+/**
+* @author hrw
+* @created 2018.8.31
+* 页面顶部组件
+**/
+<template>
+  <div class="comHeader">
+    <span>页面顶部</span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang='scss' ref='stylesheet/scss' scoped></style>
---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+/**
+* @author hrw
+* @created 2018.8.31
+**/
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import comHeader from '@/components/comment/comHeader'
+export default {
+  components: {
+    comHeader
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang='scss' ref='stylesheet/scss' scoped></style>
